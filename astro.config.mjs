55562af06a6e104a890d04f72906e51fa09fa809
Armando Urbina <urbinaa01@gmail.com>
2026-02-24 17:30:55 -0700

    first deploy to actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://urbinaarmando.github.io',
+  base: '/astro-genshin',
   vite: {
     plugins: [tailwindcss()]
   }
